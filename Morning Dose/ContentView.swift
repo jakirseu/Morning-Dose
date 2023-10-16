29cb021e764b8d7a51ac9cc1b4b30e9a9cefbc11
Jakir Hossain <jakirseu@gmail.com>
2023-10-16 18:15:10 +0600

    Initial Commit

---
+++ Morning Dose/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Morning Dose
+//
+//  Created by Jakir Hossain on 16/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Morning Dose/Morning_DoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Morning_DoseApp.swift
+//  Morning Dose
+//
+//  Created by Jakir Hossain on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Morning_DoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
